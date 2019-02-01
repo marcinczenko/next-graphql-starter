daacb0a764ffa767cfc99723a61dd856c5ad984a
hmm <hmmChase@users.noreply.github.com>
2019-02-01 13:38:55 -0800

    Update

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,28 @@
 import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  static getInitialProps = async ctx => {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      const initialProps = await Document.getInitialProps(ctx);
+
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+        });
+
+      return {
+        ...initialProps,
+        styles: [...initialProps.styles, ...sheet.getStyleElement()]
+      };
+    } finally {
+      sheet.seal();
+    }
+  };
+
   render() {
     return (
       <html>
