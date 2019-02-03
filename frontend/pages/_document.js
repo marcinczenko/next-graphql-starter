a9620bfffb9b265f2e1dbee0a60cb59451ddd5b6
hmm <hmmChase@users.noreply.github.com>
2019-02-02 16:28:51 -0800

    Update

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,7 +1,7 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
   static getInitialProps = async (ctx) => {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
@@ -34,3 +34,5 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+export default MyDocument;
